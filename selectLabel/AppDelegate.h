20db175ff1edae805eb18700e63dd7614ec414ce
vteam <vteam@vteamdeMac-mini.local>
2017-07-28 13:27:49 +0800

    Initial Commit

---
+++ selectLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  selectLabel
+//
+//  Created by vteam on 2017/7/28.
+//  Copyright © 2017年 vteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ selectLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  selectLabel
+//
+//  Created by vteam on 2017/7/28.
+//  Copyright © 2017年 vteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
